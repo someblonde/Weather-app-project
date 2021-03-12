86b2a7431edacd720fb28c470ac4d40efe4d6d43
someblonde <77970860+someblonde@users.noreply.github.com>
2021-03-12 14:06:36 +0000

    built celsius link

--- src/index.js
+++ src/index.js
@@ -70,8 +70,17 @@ function displayFahrenheitTemperature(event){
   temperatureElement.innerHTML=Math.round(fahrenheitTemperature);
 }
 
+function displayCelsiusTemperature(event){
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 let celsiusTemperature = null;
