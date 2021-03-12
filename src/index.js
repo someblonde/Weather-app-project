7706f662c0d679d45a64306a3bf713b9416c7338
someblonde <77970860+someblonde@users.noreply.github.com>
2021-03-12 14:27:20 +0000

    finished units conversion link

--- src/index.js
+++ src/index.js
@@ -65,6 +65,8 @@ currentLocationbutton.addEventListener("click", getCurrentlocation);
 
 function displayFahrenheitTemperature(event){
   event.preventDefault();
+   celsiusLink.classList.remove("active");
+   fahrenheitLink.classList.add("active");
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML=Math.round(fahrenheitTemperature);
@@ -72,6 +74,8 @@ function displayFahrenheitTemperature(event){
 
 function displayCelsiusTemperature(event){
   event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active")
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
