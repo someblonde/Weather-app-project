1800a01589b22ed135cea5da3ca3297d3370a7b1
someblonde <77970860+someblonde@users.noreply.github.com>
2021-03-14 13:59:26 +0000

    fixing time and date to work for all elements

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
-let now = new Date();
-let currentDateandTime = document.querySelector("#day-time");
-let days = [
-  `Sunday`,
-  `Monday`,
-  `Tuesday`,
-  `Wednesday`,
-  `Thursday`,
-  `Friday`,
-  `Saturday`,
-];
-let day = days[now.getDay()];
-let hours = now.getHours();
 
-let minutes = now.getMinutes();
-currentDateandTime.innerHTML = `${day} ${hours}:${minutes}`;
+
+function formatDate(timestamp) {
+  let date = new Date (timestamp);
+  
+
+  let days = 
+  ["Sunday", "Monday" ,  "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
+
+  let day = days[date.getDay()];
+  return `${day} ${formatHours(timestamp)}`;
+}
+
 
 function formatHours (timestamp){
+   let date = new Date (timestamp);
+     let hours = date.getHours();
+  if (hours < 10) {
+    hours =`0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
   return `${hours}:${minutes}`;
 
 }
@@ -36,8 +42,12 @@ function displayWeather(response) {
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
 
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
     celsiusTemperature = response.data.main.temp;
+
+    let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon");
+    
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
